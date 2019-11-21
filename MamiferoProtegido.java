e5b2724030ef41ed6f1989cf9b851cbe41238343
Micael Fischmann <micael.fischmann@agst.com.br>
2019-11-21 17:10:48 -0200

    :sparkles: Adiciona solucao Q1

---
+++ MamiferoProtegido.java
@@ -0,0 +1,21 @@
+public class MamiferoProtegido extends Mamifero implements Protegido{
+    private int anoLicenciamento;
+    private int nroLicensaIBAMA;
+
+    public MamiferoProtegido(int id, String nome, float peso, boolean pet,
+             boolean carnivoro, boolean herbivoro, int anoLicenciamento, int nroLicensaIBAMA){
+        super(id, nome, peso, pet, carnivoro, herbivoro);
+        this.anoLicenciamento = anoLicenciamento;
+        this.nroLicensaIBAMA = nroLicensaIBAMA;
+    }
+
+    @Override
+    public int getAnoLicenciamento() {
+        return anoLicenciamento;
+    }
+
+    @Override
+    public int getNroLicensaIBAMA() {
+        return nroLicensaIBAMA;
+    }
+}
