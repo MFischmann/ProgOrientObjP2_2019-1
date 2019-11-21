04dceb2d8069eb2ab7dce9e025fc5e532fb91289
Micael Fischmann <micael.fischmann@agst.com.br>
2019-11-21 11:55:47 -0200

    :sparkles: Adiciona interface Protegido

---
+++ Protegido.java
@@ -0,0 +1,4 @@
+interface Protegido {
+    public int getNroLicensaIBAMA(); // interface method (does not have a body)
+    public int getAnoLicenciamento(); // interface method (does not have a body)
+  }
