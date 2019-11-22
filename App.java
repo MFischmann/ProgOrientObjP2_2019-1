f232fb39ab455ea043cb9034edbe8a5c04c61068
Micael Fischmann <micael.fischmann@agst.com.br>
2019-11-22 11:48:45 -0200

    :sparkles: Adiciona solucao Q5

--- App.java
+++ App.java
@@ -7,16 +7,21 @@ public class App{
     public static void main(String[] args){
         try {
             List<Protegido> lstP = new ArrayList<>();
-            
-            lstP.add(new Passaro(1, "Tucano", 5, false, true, 1000, 1993));
-            lstP.add(new Passaro(2, "Pinguin", 20, false, false, 2890, 1960));
-            lstP.add(new MamiferoProtegido(3, "Tamandua", 15, false, true, false, 2000, 30));
+            Passaro ps1 = new Passaro(1, "Tucano", 5, false, true, 1000, 1993);
+            lstP.add(ps1);
+            Passaro ps2 = new Passaro(2, "Pinguin", 20, false, false, 2890, 1960);
+            lstP.add(ps2);
+            MamiferoProtegido mp1 = new MamiferoProtegido(3, "Tamandua", 15, false, true, false, 2000, 30);
+            lstP.add(mp1);
+            MamiferoProtegido mp2 = new MamiferoProtegido(90, "Rinoceronte", 300, false, false, true, 1980, 92801);
+            lstP.add(mp2);
+            System.out.println("Q2");
             System.out.println(countPassaroProt(lstP));
             Peixe p1 = new Peixe(14, "Beta", 0.1f, true, true);
             Peixe p2 = new Peixe(16, "Dourado", 0.15f, true, true);
             Mamifero m1 = new Mamifero(56, "Calico", 1.2f, true, true, false);
             Mamifero m2 = new Mamifero(33, "Coelho", 2, true, false, true);
-            Mamifero m3 = new Mamifero(27, "Pastor Alemao", 4, true, true, false);
+            Mamifero m3 = new Mamifero(27, "Pastor Alemao", 55, true, true, false);
             Animal a = new Animal(33, "Tarantula", 0.3f, true);
             Set<Animal> lojaA = new HashSet<>();
             Set<Animal> lojaB = new HashSet<>();
@@ -29,11 +34,28 @@ public class App{
             lojaB.add(m3);
             lojaB.add(m1);
             lojaB.add(p1);
-
+            List<Animal> lista = new ArrayList<>();
+            lista.add(p1);
+            lista.add(p2);
+            lista.add(m1);
+            lista.add(m2);
+            lista.add(m3);
+            lista.add(a);
+            lista.add(ps1);
+            lista.add(ps2);
+            lista.add(mp1);
+            lista.add(mp2);
+            Passaro ps3 = new Passaro(77, "Avestruz", 240, false, false, 32435, 2008);
+            lista.add(ps3);
+            System.out.println("Q4");
             Set<Animal> repetidos = animaisDuplicados(lojaA, lojaB);
             for (Animal animal : repetidos) {
                 System.out.println(animal);
             }
+
+            System.out.println("Q5");
+            maisCinquenta(lista);
+
         } 
         catch (PesoInvalidoException e) {
             System.out.println("Animal nao pode ter peso negativo.");
@@ -53,5 +75,10 @@ public class App{
             }
         }
         return dupAnimais;
+    }
+
+    public static void maisCinquenta(List<Animal> lst){
+        lst.stream().filter(a -> a.getPeso()>50).forEach(a->System.out.println(a.getNome()));
+
     }
 }
